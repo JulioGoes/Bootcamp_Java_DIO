feb3b4cc1a3b3087b45603cc3db68c1942df0959
JulioGoes <goes.cesar157@gmail.com>
2022-02-12 20:42:05 -0300

    update

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -5,7 +5,8 @@ import com.dio.base.Order;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        final Order order = new Order("Hello World");
+        final Order order = new Order("Júlio", "Eduarda");
         System.out.println(order);
+        System.out.println(1);
     }
 }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -2,16 +2,16 @@ package com.dio.base;
 
 public class Order {
 
-    private final String code;
+    private final String nome1;
+    private final String nome2;
 
-    public Order(String code){
-        this.code = code;
+    public Order(String nome1, String nome2){
+        this.nome1 = nome1;
+        this.nome2 = nome2;
     }
 
     @Override
     public String toString(){
-        return "Order={" +
-               "code'" + code + "'" +
-                "}";
+        return nome1 + nome2;
     }
 }
