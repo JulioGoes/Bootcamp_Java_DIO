a5ee7a5d2dd653776f1680d6b95b3ab900f716d3
JulioGoes <goes.cesar157@gmail.com>
2022-02-14 18:13:43 -0300

    declarando variaveis em Java

---
+++ src/com/dio/Variaveis.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+public class Variaveis {
+    public static void main(String[] args){
+        int dia = 14;
+        int mes = 02;
+        int ano = 2022;
+        double pi = 3.14;
+        float outro_pi = (float)3.14;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char primeira_letra = 'A';
+        char ultima_letra = 'Z';
+        String nome = "Júlio";
+        System.out.println("A data de hoje é: " + dia + "/" + mes + "/" + ano);
+        System.out.println("O valor de PI é: " + pi + " Ou melhor: " + outro_pi);
+        System.out.println("Verdadeiro: " + verdadeiro);
+        System.out.println("Falso: " + falso);
+        System.out.println("A primeira letra do alfabeto é: " + primeira_letra);
+        System.out.println("A última letra do alfabeto é: " + ultima_letra);
+        System.out.println("Meu nome é " + nome);
+    }
+}
