32028d45e08cf19d2eff5a5b9c22ee12ca10d052
JulioGoes <goes.cesar157@gmail.com>
2022-02-15 14:16:48 -0300

    aula 2 VTO Bootcamp

---
+++ src/com/dio/ExercicioVTO2.java
@@ -0,0 +1,42 @@
+package com.dio;
+
+public class ExercicioVTO2 {
+    public static void main(String[] args){
+        // Exercicio: Criar duas variáveis para cada tipo de dado
+        int num1 = 100;
+        int num2 = 200;
+        byte b1 = 8;
+        byte b2 = 16;
+        short s1 = 32;
+        short s2 = 64;
+        long l1 = 10000000L;
+        long l2 = 10000001L;
+        float pontoFlutuante1 = (float)3.14;
+        float pontoFlutuante2 = (float)1.6;
+        double duplo1 = 110.11;
+        double duplo2 = 112.33;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        String nome = "Júlio";
+        String sobrenome = "Goes";
+        char c1 = 'J';
+        char c2 = 'C';
+        
+        System.out.println("Inteiro: " + num1 + ", " + num2);
+        System.out.println("Byte: " + b1 + ", " + b2);
+        System.out.println("Short: " + s1 + ", " + s2);
+        System.out.println("Long: " + l1 + ", " + l2);
+        System.out.println("Floats: " + pontoFlutuante1 + ", " + pontoFlutuante2);
+        System.out.println("Double: " + duplo1 + ", " + duplo2);
+        System.out.println("Boolean: " + verdadeiro + ", " + falso);
+        System.out.println("String: " + nome + ", " + sobrenome);
+        System.out.println("Char: " + c1 + ", " + c2);
+
+
+        
+
+      
+
+
+    }
+}
--- src/com/dio/Variaveis.java
+++ src/com/dio/Variaveis.java
@@ -12,12 +12,14 @@ public class Variaveis {
         char primeira_letra = 'A';
         char ultima_letra = 'Z';
         String nome = "Júlio";
+        final int TESTANDO = 10;
         System.out.println("A data de hoje é: " + dia + "/" + mes + "/" + ano);
-        System.out.println("O valor de PI é: " + pi + " Ou melhor: " + outro_pi);
+        System.out.println("O valor de PI é: " + pi + " ou melhor: " + outro_pi);
         System.out.println("Verdadeiro: " + verdadeiro);
         System.out.println("Falso: " + falso);
         System.out.println("A primeira letra do alfabeto é: " + primeira_letra);
         System.out.println("A última letra do alfabeto é: " + ultima_letra);
         System.out.println("Meu nome é " + nome);
+        System.out.println("O valor da variável final é " + TESTANDO);
     }
 }
